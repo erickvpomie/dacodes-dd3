5cad21d0736db7d9ae2f8d8ad7f45d074b515374
Erick Pomie <erick@aocmultimedia.com>
2022-06-24 01:09:01 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dacodes-dd3
---
+++ src/app/app.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+        <h1 className="text-3xl font-bold underline text-blue-900 bg-sky-600 hover:bg-sky-700">
+            Hello world!
+        </h1>
+  )
+}
+export default App
